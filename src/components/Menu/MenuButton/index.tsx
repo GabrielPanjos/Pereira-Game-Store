9fda7086db1a0aef237e447971039c7ca2ad3285
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-09-02 23:12:33 -0300

    comecando a fazer o 'banco de dados' dos produtos

--- src/components/Menu/MenuButton/index.tsx
+++ src/components/Menu/MenuButton/index.tsx
@@ -7,7 +7,7 @@ type MenuButtonProps = {
 
 export function MenuButton({ Icon, children }: MenuButtonProps) {
   return (
-    <button className="flex flex-col gap-2 h-full justify-center items-center text-slate-100 hover:text-slate-300 font-semibold text-[15px]">
+    <button className="flex flex-col gap-2 h-full justify-center items-center text-slate-100 hover:text-slate-300 font-semibold text-[14px]">
       <Icon className="h-8 w-8" />
       <span>{children}</span>
     </button>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { MenuButton } from "./MenuButton";
 
 export function Menu() {
   return (
-    <nav className="flex justify-center items-center gap-20 w-full h-25 bg-sky-950">
+    <nav className="flex justify-center items-center gap-22 w-full h-25 bg-sky-950">
       <MenuButton Icon={Gamepad2}>Consoles</MenuButton>
       <MenuButton Icon={Disc3}>Jogos</MenuButton>
       <MenuButton Icon={Keyboard}>Periféricos</MenuButton>
