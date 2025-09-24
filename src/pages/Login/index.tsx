d368a0bbe3156914c056a51687d1ef5de486643c
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-09-24 18:13:20 -0300

    button da login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,20 +7,20 @@ export function Login() {
     <MainTemplate>
       <section className="flex items-center p-20 justify-center h-full w-full">
         <div className="flex items-center justify-center h-[800px] w-[1000px] bg-white rounded shadow-md">
-          <div className="flex flex-col gap-6 w-100">
+          <div className="flex flex-col items-center justify-center gap-6 w-100">
             <h1 className="text-[36px] text-center text-sky-950">
               Entre para continuar suas compras
             </h1>
-            <form className="mt-15 flex flex-col justify-center items-center gap-6">
+            <form className="mt-15 flex flex-col justify-center w-full items-center gap-6">
               <div className="flex flex-col gap-6 w-full mb-5">
                 <Input type="email" placeholder="Seu e-mail" />
                 <Input type="password" placeholder="Sua senha" />
                 {/* Botão de "Manter-se conectado" */}
               </div>
               <CardButton>Entrar</CardButton>
-              <span>ou</span>
-              <CardButton>Cadastrar</CardButton>
             </form>
+            <span>ou</span>
+            <CardButton>Cadastrar</CardButton>
           </div>
         </div>
       </section>
