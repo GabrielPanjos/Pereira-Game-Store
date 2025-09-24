215f9062baee07c12b53498319fb5f3e8547506a
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-09-24 18:26:50 -0300

    começando a fazer componente que mostra produtos em promoções

---
+++ src/components/ProductSpecial/index.tsx
@@ -0,0 +1,3 @@
+export function ProductSpecial() {
+  return <section></section>;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { MainTemplate } from "../../templates/MainTemplate";
 import { Products } from "../../components/Products";
+import { ProductSpecial } from "../../components/ProductSpecial";
 
 export function Home() {
   return (
     <MainTemplate>
       <div className="flex flex-col items-center">
+        <ProductSpecial />
         <Products />
       </div>
     </MainTemplate>
